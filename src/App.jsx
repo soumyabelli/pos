8c760650439d546ca06128ee73ad6e6241c013aa
Soumya yallappa belli <bellisoumya@gmail.com>
2026-04-10 23:50:34 +0530

    Added login and registration functionality

--- src/App.jsx
+++ src/App.jsx
@@ -1,48 +1,19 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Login from "./pages/Login";
-import Register from "./pages/Register";
 import AdminDashboard from "./pages/AdminDashboard";
 import ManagerDashboard from "./pages/ManagerDashboard";
 import POS from "./pages/POS";
-import ProtectedRoute from "./components/ProtectedRoute";
 
 function App() {
   return (
-    <BrowserRouter>
+    <Router>
       <Routes>
-
-        <Route path="/login" element={<Login />} />
-        <Route path="/register" element={<Register />} />
-
-        <Route
-          path="/admin"
-          element={
-            <ProtectedRoute allowedRoles={["admin"]}>
-              <AdminDashboard />
-            </ProtectedRoute>
-          }
-        />
-
-        <Route
-          path="/manager"
-          element={
-            <ProtectedRoute allowedRoles={["manager"]}>
-              <ManagerDashboard />
-            </ProtectedRoute>
-          }
-        />
-
-        <Route
-          path="/pos"
-          element={
-            <ProtectedRoute allowedRoles={["cashier"]}>
-              <POS />
-            </ProtectedRoute>
-          }
-        />
-
+        <Route path="/" element={<Login />} />
+        <Route path="/admin" element={<AdminDashboard />} />
+        <Route path="/manager" element={<ManagerDashboard />} />
+        <Route path="/pos" element={<POS />} />
       </Routes>
-    </BrowserRouter>
+    </Router>
   );
 }
 
