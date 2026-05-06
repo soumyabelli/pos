8d9c03c6786bf80ab9c2df10a600fdff011adede
Soumya yallappa belli <bellisoumya@gmail.com>
2026-05-06 16:23:03 +0530

    frontend deploy

--- src/App.jsx
+++ src/App.jsx
@@ -21,7 +21,7 @@ import ProtectedRoute from "./components/ProtectedRoute";
 
 function App() {
   return (
-    <Router>
+    <Router basename={import.meta.env.BASE_URL}>
       <Routes>
         <Route path="/" element={<Navigate to="/login" replace />} />
         <Route path="/login" element={<Login />} />
